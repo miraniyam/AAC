18b0897924423ad4612a38a4bd2d647c9a232d65
miraniyam <ranz_@naver.com>
2017-05-17 14:08:14 +0900

    뭘 바꾸었다고 써주세요~

--- app/src/main/java/com/example/miranlee/lifewithaac/MainActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Life");
     }
+
+    // 팀플 화이팅
 }
