d86522dfe85f5be00a57e9a8304c9ed4ca4975ae
sungdd2 <sungdd2@gmail.com>
2017-05-24 14:08:37 +0900

    Main 하고 있어요

--- app/src/main/java/com/example/miranlee/lifewithaac/ClothActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/ClothActivity.java
@@ -1,8 +1,10 @@
 package com.example.miranlee.lifewithaac;
 
+import android.app.Activity;
+
 /**
  * Created by miran lee on 2017-05-24.
  */
 
-public class ClothActivity {
+public class ClothActivity extends Activity{
 }
--- app/src/main/java/com/example/miranlee/lifewithaac/MainActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.miranlee.lifewithaac;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Life");
+    }
+
+    public void onClothClick(View view) {
+        Intent i = new Intent(this, ClothActivity.class);
+        startActivity(i);
     }
 }
