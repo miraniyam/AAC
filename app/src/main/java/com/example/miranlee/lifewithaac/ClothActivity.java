3b9d6afde67bf1b50516ad34355f3bd5ffe62829
miraniyam <ranz_@naver.com>
2017-05-24 13:59:35 +0900

    Cloth 하는중

--- app/src/main/java/com/example/miranlee/lifewithaac/ClothActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/ClothActivity.java
@@ -1,8 +1,18 @@
 package com.example.miranlee.lifewithaac;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by miran lee on 2017-05-24.
  */
 
-public class ClothActivity {
+public class ClothActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_cloth);
+        super.onCreate(savedInstanceState);
+        setTitle("옷가게");
+    }
 }
