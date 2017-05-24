145da6ba67ac8055e261dd7294360c3bb07d48f6
sungdd2 <sungdd2@gmail.com>
2017-05-24 14:21:19 +0900

    Cloth

---
+++ app/src/main/java/com/example/miranlee/lifewithaac/ClothActivity.java
@@ -0,0 +1,16 @@
+package com.example.miranlee.lifewithaac;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sungd on 2017-05-24.
+ */
+
+public class ClothActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_cloth);
+        super.onCreate(savedInstanceState);
+    }
+}
