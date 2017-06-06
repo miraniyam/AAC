c73f22b724af3f90710f7c85bcc433f0c59b0463
miraniyam <ranz_@naver.com>
2017-06-07 03:17:17 +0900

    옷가게 메뉴 완성! 디자인 수정이 좀 필요해보입니당..

--- app/src/main/java/com/example/miranlee/lifewithaac/ClothActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/ClothActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 /**
  * Created by sungd on 2017-05-24.
@@ -13,51 +16,174 @@ public class ClothActivity extends Activity implements TextToSpeech.OnInitListen
 
     TextToSpeech tts;
     String nar = null;
+    String cloth = "";
+    String color = "";
+    String num = "";
+    String size = "";
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_cloth);
         tts = new TextToSpeech(this,this);
+        tv = (TextView)findViewById(R.id.ClothTextView);
         super.onCreate(savedInstanceState);
     }
 
+    public String setString() {
+        // 빨간색 바지 두개 스몰로 주세요.
+        String s = color + " "+ cloth + " "+num+" ";
+        if(size != "") {
+            s = s+size+"로 주세요.";
+        }else {
+            s = s + "주세요";
+        }
+
+        if(color == "" && cloth == "" && num == "" && size == "") {
+            s = "옷, 색상, 숫자, 사이즈 버튼을 클릭하여 문장을 생성하세요.";
+        }
+        return s;
+    }
+
     public void onTshirtClick(View v) {
-        nar = "티셔츠";
+        cloth = "티셔츠";
+        nar = cloth;
         onInit(0);
     }
 
     public void onPantsClick(View v) {
-        nar = "바지";
+        cloth = "바지";
+        nar = cloth;
         onInit(0);
     }
 
     public void onSkirtClick(View v) {
-        nar = "치마";
+        cloth = "치마";
+        nar = cloth;
         onInit(0);
     }
 
     public void onShirtClick(View v) {
-        nar = "셔츠";
+        cloth = "셔츠";
+        nar = cloth;
         onInit(0);
     }
 
     public void onDressClick(View v) {
-        nar = "원피스";
+        cloth = "원피스";
+        nar = cloth;
         onInit(0);
     }
 
     public void onUnderwearClick(View v) {
-        nar = "속옷";
+        cloth = "속옷";
+        nar = cloth;
         onInit(0);
     }
 
     public void onSocksClick(View v) {
-        nar = "양말";
+        cloth = "양말";
+        nar = cloth;
+        onInit(0);
+    }
+
+    public void onRedClick(View v) {
+        color = "빨간색";
+        nar = color;
+        onInit(0);
+    }
+
+    public void onOrangeClick(View v) {
+        color = "주황색";
+        nar = color;
+        onInit(0);
+    }
+
+    public void onYellowClick(View v) {
+        color = "노란색";
+        nar = color;
+        onInit(0);
+    }
+
+    public void onGreenClick(View v) {
+        color = "초록색";
+        nar = color;
+        onInit(0);
+    }
+
+    public void onBlueClick(View v) {
+        color = "파란색";
+        nar = color;
+        onInit(0);
+    }
+
+    public void onOneClick(View v) {
+        num = "한 개";
+        nar = num;
+        onInit(0);
+    }
+
+    public void onTwoClick(View v) {
+        num = "두 개";
+        nar = num;
+        onInit(0);
+    }
+
+    public void onThreeClick(View v) {
+        num = "세 개";
+        nar = num;
+        onInit(0);
+    }
+
+    public void onFourClick(View v) {
+        num = "네 개";
+        nar = num;
+        onInit(0);
+    }
+
+    public void onFiveClick(View v) {
+        num = "다섯 개";
+        nar = num;
+        onInit(0);
+    }
+
+    public void onXSClick(View v) {
+        size = "제일 작은 사이즈";
+        nar = size;
+        onInit(0);
+    }
+
+    public void onSClick(View v) {
+        size = "작은 사이즈";
+        nar = size;
+        onInit(0);
+    }
+
+    public void onMClick(View v) {
+        size = "중간 사이즈";
+        nar = size;
+        onInit(0);
+    }
+
+    public void onLClick(View v) {
+        size = "큰 사이즈";
+        nar = size;
+        onInit(0);
+    }
+    public void onXLClick(View v) {
+        size = "제일 큰 사이즈";
+        nar = size;
+        onInit(0);
+    }
+
+    public void onSpeakClick(View v) {
+        nar = setString();
         onInit(0);
     }
 
     @Override
     public void onInit(int status) {
+        tv.setText(nar);
         tts.speak(nar, TextToSpeech.QUEUE_FLUSH,null);
     }
 }
