d1bc42fca4e6e6809c19589769bc917d966a0a9b
sungdd2 <sungdd2@gmail.com>
2017-06-07 13:23:16 +0900

    drawable

--- app/src/main/java/com/example/miranlee/lifewithaac/EmotionActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/EmotionActivity.java
@@ -13,10 +13,12 @@ import android.widget.TextView;
 public class EmotionActivity extends Activity implements TextToSpeech.OnInitListener{
     TextView tv;
     TextToSpeech tts;
+    String nar = null;
 
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_emotion);
         setTitle("emotion");
+        tts = new TextToSpeech(this, this);
         super.onCreate(savedInstanceState);
 
 
@@ -26,55 +28,79 @@ public class EmotionActivity extends Activity implements TextToSpeech.OnInitList
     public void onangryClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("angry");
+        nar = "화났어요";
+        onInit(0);
     }
     public void onbashfulClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("bashful");
+        nar = "부끄러워요";
+        onInit(0);
     }
     public void onghostClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("ghost");
+        nar = "오싹해요";
+        onInit(0);
     }
     public void onhappyClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("happy");
+        nar = "행복해요";
+        onInit(0);
     }
     public void onunhappyClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("unhappy");
+        nar = "불행해요";
+        onInit(0);
     }
     public void onloveClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("love");
+        nar = "좋아해요";
+        onInit(0);
     }
     public void onnumbClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("numb");
+        nar = "망연자실해요";
+        onInit(0);
     }
     public void onpleasedClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("pleased");
+        nar = "즐거워요";
+        onInit(0);
     }
     public void onsadClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("sad");
+        nar = "우울해요";
+        onInit(0);
     }
     public void onshockClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("shock");
+        nar = "충격이에요";
+        onInit(0);
     }
     public void onunsureClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("unsure");
+        nar = "자신이없어요";
+        onInit(0);
     }
     public void onupsetClick(View view) {
         tv=(TextView)findViewById(R.id.see);
         tv.setText("upset");
+        nar = "속상해요";
+        onInit(0);
     }
 
     @Override
     public void onInit(int i) {
-
+        tts.speak(nar,TextToSpeech.QUEUE_FLUSH, null);
     }
 }
 
