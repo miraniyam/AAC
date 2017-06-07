b910d42d8373089f3898b21a41cebda17ffeaf08
hojingong <rhdghwls5@naver.com>
2017-06-07 23:32:22 +0900

    나만의 AAC를 정말 완성

--- app/src/main/java/com/example/miranlee/lifewithaac/CustomActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/CustomActivity.java
@@ -1,14 +1,25 @@
 package com.example.miranlee.lifewithaac;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +28,22 @@ import java.util.List;
  */
 
 public class CustomActivity extends Activity{
-GridView gridView;
+
+    GridView gridView;
+    MediaPlayer mediaPlayer;
+    MyAACDB myAACDB;
+    SQLiteDatabase db;
+
+    GridAdapter adapter;
+    //final List<String> list = new ArrayList<String>();
+    //final ArrayAdapter<String> gridViewArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,list);
+    //ArrayList<Bitmap> photoArr = new ArrayList<Bitmap>();
+    //ArrayList<String>textArr= new ArrayList<String>();
+    //ArrayList<String>voiceArr=new ArrayList<String>();
+
+    ArrayList<AAC> aac = new ArrayList<AAC>();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_custom);
         setTitle("myAAC");
@@ -36,11 +62,28 @@ GridView gridView;
     void init(){
         gridView = (GridView)findViewById(R.id.myAACgrid);
         Button btn = (Button)findViewById(R.id.cutomplus);
+        adapter = new GridAdapter(this,aac);
+        gridView.setAdapter(adapter);
 
-        final List<String> list = new ArrayList<String>();
-        final ArrayAdapter<String> gridViewArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,list);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                AAC item = (AAC)adapter.getItem(pos);
 
-        gridView.setAdapter(gridViewArrayAdapter);
+                    mediaPlayer = new MediaPlayer();
+                    try {
+                        mediaPlayer.setDataSource(item.getMvoice());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        mediaPlayer.prepare();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    mediaPlayer.start();
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,18 +91,64 @@ GridView gridView;
 
                 //추가ㅏ화면으로 넘어가기
                 Intent intent = new Intent(getApplicationContext(), CustomPlusActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent,1);//requestcode==1
 
-
-                list.add(list.size(),"what?");
-                gridViewArrayAdapter.notifyDataSetChanged();
-
-                String addedItemText = list.get(list.size()-1);
-
-                Toast.makeText(getApplicationContext(),"AAC added"+addedItemText,Toast.LENGTH_SHORT).show();
             }
         });
+
+        showList();
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1){
+            Toast.makeText(getApplicationContext(),"AAC added",Toast.LENGTH_SHORT).show();
+            //
+            if(resultCode==RESULT_OK){ //넣기 성공
+                showList(); //목록보여주기
+            }
+        }
+    }
 
+    void showList(){
+        //리스트 갱신
+        aac.clear();
+        myAACDB = new MyAACDB(this);
+        db = myAACDB.getReadableDatabase();
+        myAACDB.onCreate(db);
+        final MyAACHandler dbhandler = new MyAACHandler(getApplicationContext());
+        Cursor cursor = db.rawQuery("SELECT * FROM MyAAC",null);
+        int count = cursor.getCount();
+
+        while(cursor.moveToNext()){
+            String voice = cursor.getString(0); //voice
+            byte[] image = cursor.getBlob(1);//image
+            String textt = cursor.getString(2);//text
+
+           // photoArr.add(getImage(image));
+           // textArr.add(textt);
+           // voiceArr.add(voice);
+
+            adapter.ADDAAC(new AAC(textt,voice,getImage(image)));
+        }
+        cursor.close();
+       // gridViewArrayAdapter.notifyDataSetChanged();
+//        reviewAdapter.notifyDataSetChanged();
+
+        //
+        //imageView.setImageBitmap(getImage(image));
+        adapter.notifyDataSetChanged();
+
+    }
+
+//바이트어레이를 비트맵으로
+    public static Bitmap getImage(byte[] image){
+        return BitmapFactory.decodeByteArray(image,0,image.length);
+    }
+
+    //온클릭 달아서 누르면 소리나오게
+    //경로 가져와서 하기...
+    //play함수 응용
 }
