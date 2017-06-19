a36bd74db7bdbde33c182b5c5cb885d4ed800068
miraniyam <ranz_@naver.com>
2017-06-19 17:41:08 +0900

    뭐지..?

--- app/src/main/java/com/example/miranlee/lifewithaac/HospitalActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/HospitalActivity.java
@@ -1,16 +1,31 @@
 package com.example.miranlee.lifewithaac;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by sungd on 2017-05-28.
  */
 
-public class HospitalActivity extends Activity{
+public class HospitalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_hospital);
-        setTitle("hospital");
+        setTitle("Game");
         super.onCreate(savedInstanceState);
+    }
+
+    public void onClickLevel1(View v) {
+        startActivity(new Intent(this, LevelOneActivity.class));
+    }
+
+    public void onClickLevel2(View v) {
+        startActivity(new Intent(this, LevelTwoActivity.class));
+    }
+
+    public void onClickLevel3(View v) {
+        startActivity(new Intent(this, LevelThreeActivity.class));
     }
 }
--- app/src/main/java/com/example/miranlee/lifewithaac/LevelOneActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/LevelOneActivity.java
@@ -1,8 +1,76 @@
 package com.example.miranlee.lifewithaac;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+
 /**
  * Created by miran lee on 2017-06-19.
  */
 
-public class LevelOneActivity {
+public class LevelOneActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
+
+    TextToSpeech tts;
+    String nar = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_level1);
+        setTitle("Level 1");
+        tts = new TextToSpeech(this, this);
+        super.onCreate(savedInstanceState);
+    }
+
+    public void onClick1(View v) {
+        nar = "일";
+        onInit(0);
+    }
+
+    public void onClick2(View v) {
+        nar = "이";
+        onInit(0);
+    }
+
+    public void onClick3(View v) {
+        nar = "삼";
+        onInit(0);
+    }
+
+    public void onClick4(View v) {
+        nar = "사";
+        onInit(0);
+    }
+
+    public void onClick5(View v) {
+        nar = "오";
+        onInit(0);
+    }
+
+    public void onClick6(View v) {
+        nar = "육";
+        onInit(0);
+    }
+
+    public void onClick7(View v) {
+        nar = "칠";
+        onInit(0);
+    }
+
+    public void onClick8(View v) {
+        nar = "팔";
+        onInit(0);
+    }
+
+    public void onClick9(View v) {
+        nar = "구";
+        onInit(0);
+    }
+
+    @Override
+    public void onInit(int status) {
+        tts.speak(nar,TextToSpeech.QUEUE_FLUSH,null);
+    }
 }
