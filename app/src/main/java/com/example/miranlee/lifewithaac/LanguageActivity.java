3c7e4173aa608147ec228d17293ff5aa70b21c96
miraniyam <ranz_@naver.com>
2017-06-20 01:49:28 +0900

    언어 부분 파일 입출력 완료

--- app/src/main/java/com/example/miranlee/lifewithaac/LanguageActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/LanguageActivity.java
@@ -1,16 +1,125 @@
 package com.example.miranlee.lifewithaac;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 /**
  * Created by sungd on 2017-05-28.
  */
 
-public class LanguageActivity extends Activity{
+public class LanguageActivity extends Activity {
+
+    String lang = null;
+    String path = null;
+    Button korean, english;
+
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
         setTitle("language");
-        super.onCreate(savedInstanceState);
+
+        Intent i = getIntent();
+        lang = i.getStringExtra("lang");
+        path = i.getStringExtra("path");
+
+        korean = (Button)findViewById(R.id.korean);
+        english = (Button)findViewById(R.id.english);
+
+        if(lang.equals("korean")) {
+            korean.setClickable(false);
+            korean.setEnabled(false);
+        }
+        if(lang.equals("english")) {
+            english.setClickable(false);
+            english.setEnabled(false);
+        }
+    }
+
+    public void onClickKorean(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(LanguageActivity.this);
+        alert.setMessage("사용 언어를 한국어로 변환하시겠습니까?").setCancelable(true).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                File file = new File(path);
+                FileWriter fw = null;
+                BufferedWriter buf = null;
+                try {
+                    // 기본은 한국어로 설정.
+                    fw = new FileWriter(file);
+                    buf = new BufferedWriter(fw);
+                    buf.write("korean");
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    if(buf != null) {
+                        buf.close();
+                    }
+                    if(fw != null) {
+                        fw.close();
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(LanguageActivity.this, MainActivity.class));
+                finish();
+            }
+        }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog a = alert.create();
+        a.show();
+    }
+
+    public void onClickEnglish(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(LanguageActivity.this);
+        alert.setMessage("Do you want to change your language to English?").setCancelable(true).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                File file = new File(path);
+                FileWriter fw = null;
+                BufferedWriter buf = null;
+                try {
+                    fw = new FileWriter(file);
+                    buf = new BufferedWriter(fw);
+                    buf.write("english");
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    if(buf != null) {
+                        buf.close();
+                    }
+                    if(fw != null) {
+                        fw.close();
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(LanguageActivity.this, MainActivity.class));
+                finish();
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog a = alert.create();
+        a.show();
     }
 }
--- app/src/main/java/com/example/miranlee/lifewithaac/MainActivity.java
+++ app/src/main/java/com/example/miranlee/lifewithaac/MainActivity.java
@@ -1,40 +1,100 @@
 package com.example.miranlee.lifewithaac;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class MainActivity extends Activity {
+
+    String lang = "";
+    String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        File files = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Documents/LifeWithAAC");
+        boolean f = files.mkdirs();
+        File file = new File(files.getAbsolutePath()+"/lang.txt");
+        path = file.getAbsolutePath();
+
+        if(!file.exists()) {
+            FileWriter fw = null;
+            BufferedWriter buf = null;
+            try {
+                // 기본은 한국어로 설정.
+                fw = new FileWriter(file);
+                buf = new BufferedWriter(fw);
+                buf.write("korean");
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if(buf != null) {
+                    buf.close();
+                }
+                if(fw != null) {
+                    fw.close();
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else {
+            char ch;
+            int data;
+            FileReader fr = null;
+            try {
+                fr = new FileReader(file);
+                while((data = fr.read()) != -1) {
+                    ch = (char)data;
+                    lang = lang+ch;
+                }
+                fr.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     public void onClothClick(View view) {
         Intent i = new Intent(this, ClothActivity.class);
+        i.putExtra("lang",lang);
         startActivity(i);
     }
     public void onEmotionClick(View view) {
         Intent i = new Intent(this, EmotionActivity.class);
+        i.putExtra("lang",lang);
         startActivity(i);
     }
     public void onHospitalClick(View view) {
         Intent i = new Intent(this, GameActivity.class);
+        i.putExtra("lang",lang);
         startActivity(i);
     }
     public void onLanguageClick(View view) {
         Intent i = new Intent(this, LanguageActivity.class);
+        i.putExtra("lang",lang);
+        i.putExtra("path",path);
         startActivity(i);
     }
     public void onRestaurantClick(View view) {
         Intent i = new Intent(this, CustomActivity.class);
+        i.putExtra("lang",lang);
         startActivity(i);
     }
     public void onSosClick(View view) {
         Intent i = new Intent(this, SosActivity.class);
+        i.putExtra("lang",lang);
         startActivity(i);
     }
 }
